6e8abf05d4a160a999ac3d60a5df97de018ccda0
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-01-30 13:22:44 -0600

    Starting auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
+          {/* <Navbar /> */}
           {children}
         </ThemeProvider>
       </body>
