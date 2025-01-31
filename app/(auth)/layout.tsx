42af8c56627995c51cbbb4d83fe2e611de61a2a0
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-01-31 17:55:57 -0600

    Authjs setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,24 +1,33 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 
-// import LeftSidebar from "@/components/navigation/LeftSidebar";
-// import RightSidebar from "@/components/navigation/RightSidebar";
+// import SocialAuthForm from "@/components/forms/SocialAuthForm";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>{children}</main>
-    // <main className="background-light850_dark100 realtive">
-    //   <Navbar />
+    <main className="flex min-h-screen items-center justify-center bg-auth-light bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-auth-dark">
+      <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+        <div className="flex items-center justify-between gap-2">
+          <div className="space-y-2.5">
+            <h1 className="h2-bold text-dark100_light900">Join DevFlow</h1>
+            <p className="paragraph-regular text-dark500_light400">
+              To get your questions answered
+            </p>
+          </div>
+          <Image
+            src="images/site-logo.svg"
+            alt="DevFlow Logo"
+            width={50}
+            height={50}
+            className="object-contain"
+          />
+        </div>
 
-    //  <div className="flex">
-    //     <LeftSidebar />
+        {children}
 
-    //     <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-    //       <div className="mx-auto w-full max-w-5xl">{children}</div>
-    //     </section>
-
-    //     <RightSidebar />
-    //   </div>
-    // </main>
+        {/* <SocialAuthForm /> */}
+      </section>
+    </main>
   );
 };
 
