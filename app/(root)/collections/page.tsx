a63ff0d4e9632d35ac173590e966f25ba0bfeacf
Md Rashidul Alam Sami <rashidul321alam@gmail.com>
2025-02-02 16:47:50 -0600

    Mobile Navbar

---
+++ app/(root)/collections/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>collections</div>;
+};
+
+export default page;
---
+++ app/(root)/community/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Comm</div>;
+};
+
+export default page;
